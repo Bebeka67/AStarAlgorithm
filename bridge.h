e383e5405da25b42bf21e7846ff95d9e8470c93a
Bebeka67 <andpa98@gmail.com>
2022-08-24 13:31:07 +0300

    Added dimention Q_PROPERTY's of the matrix

--- bridge.h
+++ bridge.h
@@ -9,9 +9,29 @@ class Bridge : public QObject
 public:
     explicit Bridge(QObject *parent = nullptr);
 
+    Q_PROPERTY(int column READ column WRITE setColumn NOTIFY columnChanged)
+    Q_PROPERTY(int row READ row WRITE setRow NOTIFY rowChanged)
+
+
+    int column() const;
+    int row() const;
+
+
 signals:
 
+    void columnChanged(int column);
+    void rowChanged(int row);
+
 public slots:
+
+    void setColumn(int column);
+    void setRow(int row);
+
+private:
+
+    int m_column;
+    int m_row;
+
 };
 
 #endif // BRIDGE_H
